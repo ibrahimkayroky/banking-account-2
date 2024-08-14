d0a87628422ad06414cf3107154643705320abda
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2024-08-15 00:16:42 +0300

    update admin.java
    admin

--- src/main/java/com/example/banking_app/Classes/Admin.java
+++ src/main/java/com/example/banking_app/Classes/Admin.java
@@ -1,4 +1,22 @@
 package com.example.banking_app.Classes;
 
 public class Admin {
+    private ClientList clientList;
+
+    public Admin(ClientList clientList) {
+        this.clientList = clientList;
+    }
+
+    public void addClient(Client client) {
+        clientList.addClient(client);
+    }
+
+    public void removeClient(int accountNumber) {
+        clientList.removeClient(accountNumber);
+    }
+
+    public void showAllClients() {
+        clientList.showALLClients();
+    }
+
 }
