fc7243a6c69d0421c92d5fe12aee84b51ae9a0c5
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2024-08-15 00:19:22 +0300

    requestQueue
    requestQueue

--- src/main/java/com/example/banking_app/Classes/RequestQueue.java
+++ src/main/java/com/example/banking_app/Classes/RequestQueue.java
@@ -1,4 +1,32 @@
 package com.example.banking_app.Classes;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class RequestQueue {
+    private Queue<String> requestQueue;
+
+    public RequestQueue() {
+        requestQueue = new LinkedList<>();
+    }
+
+    public void addRequest(String request) {
+        requestQueue.add(request);
+    }
+
+    public void processNextRequest() {
+        if (!requestQueue.isEmpty()) {
+            String request = requestQueue.poll(); //remove head
+            System.out.println("Processing " + request);
+        }
+        else {
+            System.out.println("No requests in the queue.");
+        }
+    }
+
+    public void showRequests() {
+        for (String request : requestQueue) {
+            System.out.println(request);
+        }
+    }
 }
