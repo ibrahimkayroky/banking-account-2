08181f0ffadcdf795ce81fd6630d818031f960b1
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2024-08-15 00:11:44 +0300

    update transstack.java
    transStack

--- src/main/java/com/example/banking_app/Classes/TransactionStack.java
+++ src/main/java/com/example/banking_app/Classes/TransactionStack.java
@@ -1,4 +1,31 @@
 package com.example.banking_app.Classes;
 
+import java.util.Stack;
+
 public class TransactionStack {
+    private Stack<Transaction> transactionStack;
+
+    public TransactionStack()
+    {
+        transactionStack = new Stack<>();
+    }
+
+    public void pushTransaction(Transaction transaction)
+    {
+        transactionStack.push(transaction);
+    }
+
+    public void popTransaction()
+    {
+        if(!transactionStack.isEmpty())
+        {
+            Transaction lastTransaction = transactionStack.pop();
+            System.out.println(lastTransaction);
+        }
+
+        else {
+            System.out.println("not found");
+        }
+    }
+
 }
